92fbe1da4078ff527eaf6cf39c726f6af1f6e159
Jan Szumiec <jan.szumiec@gmail.com>
2011-03-03 11:49:15 +0100

    Plugin is now capable of collecting all information and graphing it accordingly.

--- beanstalkd.py
+++ beanstalkd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 from beanstalk import serverconn
 
@@ -7,15 +9,27 @@ def connect(host='localhost', port=11300):
     return serverconn.ServerConn(host, port)
 
 def config():
-    print_config()
+    c = connect()
+    tubes = c.list_tubes()['data']
+    print_config(tubes)
 
-def print_config(graph_title='Beanstalkd jobs', graph_vlabel='count'):
+def print_config(tubes, graph_title='Beanstalkd jobs', graph_vlabel='count'):
     print 'multigraph job_count'
     print 'graph_order ' + ' '.join(STATES)
     print 'graph_title ' + graph_title
     print 'graph_vlabel ' + graph_vlabel
     for state in STATES:
         print '%s.label %s' % (state, state,)
+    print
+
+    for tube in tubes:
+        print 'multigraph job_count_' + tube
+        print 'graph_title %s (%s)' % (graph_title, tube,)
+        print 'graph_order ' + ' '.join(STATES)
+        print 'graph_vlabel ' + graph_vlabel
+        for state in STATES:
+            print '%s.label %s' % (state, state,)
+        print
     
 def run():
     c = connect()
@@ -24,12 +38,13 @@ def run():
 
 def print_values(tubes, c):
     for tube in tubes:
-        print 'multigraph job_count.' + tube
+        print 'multigraph job_count_' + tube
         stats = c.stats_tube(tube)['data']
         for state in STATES:
             key = 'current-jobs-' + state
             value = stats[key]
             print '%s.value %d' % (state, value,)
+        print
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "config":
